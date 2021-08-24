2a33a87856836723ddc19b61cb84fbdb8c6b8667
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-24 11:11:54 -0300

    Modificando as coisas

--- Assets/Js/make.js
+++ Assets/Js/make.js
@@ -1,56 +1,21 @@
 $(function () {
-
-    // Inicializando com uma framework
-    function aOs() {
-        let x = AOS.init();
-        console.log(x);
-    }
-    aOs();
-
-    // Slider
-    function slider(element) {
-        var owl = $(element);
-        owl.owlCarousel({
-            loop: true,
-            margin: 5,
-            smartSpeed: 2000,
-            nav: true,
-            autoplay: true,
-            autoplayTimeout: 2000,
-            autoplayHoverPause: true,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                600: {
-                    items: 3
-                },
-                1000: {
-                    items: 3
-                }
-            }
-        })
-    }
-    slider('.owl-carousel');
-
-    // Renderizar os testemunhos
-    function renderTestemunhos() {
-
-    }
-    renderTestemunhos();
-
-    // ScrollTop
-    function topHeaderScroll() {
-        $(window).scroll(function () {
-            let top = $(this).scrollTop();
-            if (top > 100) {
-                $("#topScroll").css("display", "block")
-            } else {
-                $("#topScroll").css("display", "none")
-            }
-        });
-    }
-    topHeaderScroll();
-
-
+  !(function () {
+    let o = AOS.init();
+    console.log(o);
+  })(),
+    $(".owl-carousel").owlCarousel({
+      loop: !0,
+      margin: 5,
+      smartSpeed: 2e3,
+      nav: !0,
+      autoplay: !0,
+      autoplayTimeout: 2e3,
+      autoplayHoverPause: !0,
+      responsive: { 0: { items: 1 }, 600: { items: 3 }, 1000: { items: 3 } },
+    }),
+    $(window).scroll(function () {
+      $(this).scrollTop() > 100
+        ? $("#topScroll").css("display", "block")
+        : $("#topScroll").css("display", "none");
+    });
 });
